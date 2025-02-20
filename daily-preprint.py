395cbc15b730305b6146ff84d096e20c372874a3
Ruiyang Zhou <fentaniao@gmail.com>
2025-02-20 14:10:52 +0800

    Update daily-preprint.py

--- daily-preprint.py
+++ daily-preprint.py
@@ -8,7 +8,6 @@ load_dotenv(".env")
 USER = os.environ.get("GMAIL_USER")
 PASSWORD = os.environ.get("GMAIL_PASSWORD")
 RECIPIENT = os.environ.get("RECIPIENT")
-subscriptions = ["math.AT","math.AG"]
-
+subscriptions = ["quant-ph", ]
 
 send_email(RECIPIENT,subscriptions,USER,PASSWORD)
